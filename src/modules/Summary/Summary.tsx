46b1c656e90b66d3ba26608c4e39ca4243cfa133
levente kiraly <leventekk@users.noreply.github.com>
2023-01-08 20:59:57 +0100

    fix(summary): fix layout on mobile

--- src/modules/Summary/Summary.tsx
+++ src/modules/Summary/Summary.tsx
@@ -1,12 +1,18 @@
 import useSimulator from '@hook/useSimulator'
 import { styled } from 'styled-components'
 import formatCurrency from '@helper/formatCurrency'
+import media from '@helper/media'
 
 const Wrapper = styled.div`
-  align-items: center;
   display: flex;
+  flex-direction: column;
   flex-wrap: wrap;
   gap: 1rem;
+
+  ${media.greaterThan('desktop')`
+    align-items: center;
+    flex-direction: row;
+  `}
 `
 
 const Price = styled.span`
